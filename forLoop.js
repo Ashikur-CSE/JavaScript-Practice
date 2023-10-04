cf54d0cce2269180de0dbad48e902d181f5bf10d
Ashikur-CSE <ashikur201.cse@gmail.com>
2023-10-04 20:42:13 +0600

    Basic JavaScript

---
+++ forLoop.js
@@ -0,0 +1,19 @@
+
+// for (var i=0; i<=10; i++){
+//     console.log("I love You : " + i);
+// }
+
+//1+2+3+4+5+6+7+8+9+......+100
+// var sum = 0;
+// m= parseInt(prompt("Enter a number: "));
+// for (var i = 0; i<=m; i++){
+//     sum=sum + i;
+// }
+// console.log("The sum of 1 to 100 is: " + sum);
+
+const cars = ["BMW", "Volvo", "Saab", "Ford"];
+let i, len, text;
+for (i = 0, len = cars.length, text = ""; i < len; i++) {
+  text += cars[i];
+}
+console.log(text);
---
+++ whileLoop.js
@@ -0,0 +1,21 @@
+// var num = 0;
+
+// while (num <= 1000){
+//     console.log("I Love You:" + num);
+//     num++;
+// }
+
+//print all evan number from 1 to 1000
+var num = 2;
+while(num <= 1000){
+    console.log(num);
+    num=num+2;
+}
+
+//do while loop
+var num = 1;
+do {
+    console.log("Number is: " + num);
+    num++;
+
+}while(num <= 20)
