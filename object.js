0de4a21d5f23238dd5d16d36e7021d6ffda8cb94
Ashikur-CSE <ashikur201.cse@gmail.com>
2023-10-05 16:19:18 +0600

    Object

---
+++ object.js
@@ -0,0 +1,19 @@
+var student = {
+    name : "Ashikur",
+    age : 22,
+    color : "White",
+    height : "160 feet"
+};
+
+//print value
+console.log(student);
+console.log(student.name);
+console.log(student["age"]);
+
+//set object value  
+student.name = "Ashikur Rahman";
+student["age"] = 25;
+
+console.log(student);
+console.log(student.name);
+console.log(student["age"]);
