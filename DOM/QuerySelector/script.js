5aca5be99df254c50e538ec143ce26462642b830
Ashikur-CSE <ashikur201.cse@gmail.com>
2023-10-30 02:03:22 +0600

    querySelectorAll

---
+++ DOM/QuerySelector/script.js
@@ -0,0 +1,11 @@
+var bdy = document.querySelector('.sec');
+bdy.style.backgroundColor = 'gray';
+bdy.style.padding = '20px';
+bdy.style.border = '5px solid green';
+bdy.style.borderRadius = '10px';
+
+
+var all = document.querySelectorAll('.blog');
+for (const item of all){
+    item.style.fontSize = '30px'
+}
