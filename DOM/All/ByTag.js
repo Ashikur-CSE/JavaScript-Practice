0519625b60931ce70fbdd6c302406e9ab14696d1
Ashikur-CSE <ashikur201.cse@gmail.com>
2023-10-29 17:11:04 +0600

    Style with js

--- DOM/All/ByTag.js
+++ DOM/All/ByTag.js
@@ -1,5 +1,9 @@
 var h3tag = document.getElementsByTagName("h3")
 for (const h3 of h3tag){
     document.write(h3.innerText)
-    console.log(h3.innerText)
+    
 }
+
+var tag2 = document.getElementsByTagName("h3")[1];
+tag2.innerHTML = "This blog is edited by JS";
+
---
+++ DOM/style with js/script.js
@@ -0,0 +1,9 @@
+var firstB = document.getElementById("first");
+firstB.style.backgroundColor = "yellow"
+firstB.style.padding = "10px";
+firstB.style.textAlign = "center";
+firstB.style.fontSize = '20px'
+
+var secondB = document.getElementsByClassName("second");
+secondB[0].style.backgroundColor = "red";
+secondB[0].style.padding = '50px' , color = 'green';
