7430c337a9947115026dfefbe628771bd717cb03
Ashikur-CSE <ashikur201.cse@gmail.com>
2024-02-05 14:40:38 +0600

    Template String

---
+++ ES6/template-string.js
@@ -0,0 +1,11 @@
+const details = `I am Md. Ashikur Rahman.
+I am a CSE Engineer.
+I compleated my graduation from Dafodil International University `
+console.log(details)
+
+const first= "Ashikur"
+const last = "Rahman"
+console.log(`My name is: ${first} ${last}`)
+
+const count = 5
+console.log(`Count is: ${count}`)
